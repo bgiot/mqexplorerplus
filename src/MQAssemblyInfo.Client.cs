7743c9e0bb0d0e964814e5e71575f093b0407ce0
Bertrand Giot <bgiot@dotconsulting.be>
2018-04-27 19:54:34 +0200

    Prepare version 1.0.1 for build release

--- src/MQAssemblyInfo.Client.cs
+++ src/MQAssemblyInfo.Client.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Resources;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.1")]
+[assembly: AssemblyInformationalVersion("1.0.1")]
 
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en")]
