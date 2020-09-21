6b178649bca8a257eeab8f63866fe2dac434d999
Bertrand Giot <bgiot@dotconsulting.be>
2020-09-21 21:10:35 +0200

    Upgrade to .net 4.8 and update all packages

--- src/InfraAssemblyInfo.cs
+++ src/InfraAssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Reflection;
 using System.Resources;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0")]
 
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en")]
