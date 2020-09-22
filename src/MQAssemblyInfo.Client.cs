cb24a230cf6eb16f8cdd224fa35ca05f981aae30
Bertrand Giot <bgiot@dotconsulting.be>
2020-09-22 21:13:39 +0200

    Fix old bug linked to migration to open-source

--- src/MQAssemblyInfo.Client.cs
+++ src/MQAssemblyInfo.Client.cs
@@ -3,8 +3,8 @@ using System.Reflection;
 using System.Resources;
 
 [assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.2")]
-[assembly: AssemblyInformationalVersion("1.0.2")]
+[assembly: AssemblyFileVersion("1.0.3")]
+[assembly: AssemblyInformationalVersion("1.0.3")]
 
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en")]
