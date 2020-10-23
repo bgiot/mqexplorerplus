c9f9b1be1ed50b6590e5819d117f574a9a72eded
Bertrand Giot <bgiot@dotconsulting.be>
2020-10-23 17:42:52 +0200

    Fix setup

--- src/MQAssemblyInfo.Client.cs
+++ src/MQAssemblyInfo.Client.cs
@@ -4,7 +4,7 @@ using System.Resources;
 
 [assembly: AssemblyVersion("1.1.0.0")]
 [assembly: AssemblyFileVersion("1.1.0")]
-[assembly: AssemblyInformationalVersion("1.1.0")]
+[assembly: AssemblyInformationalVersion("1.1.0.297")]
 
 [assembly: CLSCompliant(false)]
 [assembly: NeutralResourcesLanguage("en")]
